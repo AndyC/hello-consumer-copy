22583f78ac0a56b73622b59873afc6dbb7413c83
AndyCui <13520061695@163.com>
2018-01-11 14:32:56 +0800

    去掉不需要的代码

--- src/main/java/person/andy/controller/HelloConsumerController.java
+++ src/main/java/person/andy/controller/HelloConsumerController.java
@@ -24,43 +24,6 @@ public class HelloConsumerController {
     }
     @RequestMapping(value = "/serviceHello",method = RequestMethod.GET)
     public String helloAsService(HttpServletRequest request){
-        String remoteIp=getClientIp(request);
         return "hello as service";
-    }
-    private static String getClientIp(HttpServletRequest request) {
-        String ip = request.getHeader("X-Forwarded-For");
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getHeader("Proxy-Client-IP");
-
-            }
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getHeader("WL-Proxy-Client-IP");
-
-            }
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getHeader("HTTP_CLIENT_IP");
-
-            }
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-
-            }
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getRemoteAddr();
-
-            }
-        } else if (ip.length() > 15) {
-            String[] ips = ip.split(",");
-            for (int index = 0; index < ips.length; index++) {
-                String strIp = (String) ips[index];
-                if (!("unknown".equalsIgnoreCase(strIp))) {
-                    ip = strIp;
-                    break;
-                }
-            }
-        }
-        System.err.println("最终获得的客户端ip是{}"+ip);
-        return ip;
     }
 }
